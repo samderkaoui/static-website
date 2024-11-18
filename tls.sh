45d2a6becf77c0751ebefb14180fe267130fc61c
Samir DERKAOUI <126152143+samderkaoui@users.noreply.github.com>
2024-11-18 13:56:57 +0100

    Update tls.sh

--- tls.sh
+++ tls.sh
@@ -1,5 +1,5 @@
 openssl req -new -newkey rsa:2048 -days 36500 -nodes -x509 \
--keyout ./selfsigned.key \
--out ./selfsigned.crt \
--subj "//CN=hostname" \
+-keyout selfsigned.key \
+-out selfsigned.crt \
+-subj "/CN=hostname" \
 -addext "subjectAltName = IP:127.0.0.1, DNS:localhost"
